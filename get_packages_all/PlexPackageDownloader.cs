abbffcfd97205aeb50596ef035f5171273d6b00c
pavel pesetskiy <pavel_pesetskiy@epam.com>
2016-09-22 16:07:02 +0300

    init

---
+++ get_packages_all/PlexPackageDownloader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet;
+
+namespace CleanNugetSharp
+{
+  public class PlexPackageDownloader
+  {
+    private Uri _uri;
+    private string _downloadfolder;
+
+    public PlexPackageDownloader(Uri uri, string downloadfolder)
+    {
+      _uri = uri;
+      _downloadfolder = downloadfolder;
+    }
+    public void Download(IPackage package)
+    {
+      try
+      {
+        //NuGet.PackageDownloader downloader = new NuGet.PackageDownloader();
+
+        var filename = String.Format("{0}.{1}.nupkg", package.Id, package.Version);
+        var url = String.Format("{0}/{1}/{2}", _uri, package.Id, package.Version);
+        var path = Path.Combine(_downloadfolder, filename);
+        //var stream = new System.IO.FileStream(path, FileMode.Create);
+
+        //downloader.DownloadPackage(_uri, package, stream);
+        using (var client = new WebClient())
+        {
+            client.DownloadFile(url, path);
+        }
+        //downloader.ProgressAvailable += handler;
+      }
+      catch (Exception ex)
+      {
+        throw new Exception(string.Format("Download package {0} version {1} failed", package.Id, package.Version),ex);
+      }
+
+    }
+
+    private void handler(object obj, ProgressEventArgs args)
+    {
+      Console.WriteLine(args.PercentComplete);
+    }
+
+  }
+}
---
+++ get_packages_all/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanNugetSharp;
+using NuGet;
+
+namespace get_packages_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connect to the official package repository
+            //var uri = "http://nuget.plex-dev.projects.epam.com/api/";
+            var uri = "https://packages.plex.com/nuget";
+            var uri2 = "https://packages.plex.com/api/v2/package";
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository(uri);
+
+            //Get the list of all NuGet packages with ID 'EntityFramework'       
+            List<IPackage> packages = repo.GetPackages().ToList();
+            var downloader = new PlexPackageDownloader((Uri)new Uri(uri2), "d:\\packages");
+            foreach (var package in packages)
+            {
+                Console.WriteLine(package.GetFullName());
+                downloader.Download(package);
+                //Console.ReadLine();
+            }
+            
+        }
+    }
+}
